6751dfbb59c66734abbff1105694fc86a0bd91aa
Omar Ismail <oismail91@gmail.com>
2021-10-11 17:44:44 -0400

    Backend State Migration from cloud to cloud.
    * Add test for tfc to tfc mgiration
    * Fix old tests, and remove unused code.

--- internal/command/meta_backend_migrate.go
+++ internal/command/meta_backend_migrate.go
@@ -532,12 +532,6 @@ func (m *Meta) backendMigrateTFC(opts *backendMigrateOpts) error {
 		return fmt.Errorf(strings.TrimSpace(errTFCMigrateNotYetImplemented))
 	}
 
-	// from TFC to TFC
-	if sourceTFC && destinationTFC {
-		// TODO: see internal/cloud/e2e/migrate_state_tfc_to_tfc_test.go for notes
-		panic("not yet implemented")
-	}
-
 	// Everything below, by the above two conditionals, now assumes that the
 	// destination is always Terraform Cloud (TFC).
 
@@ -644,6 +638,13 @@ func (m *Meta) backendMigrateState_S_TFC(opts *backendMigrateOpts, sourceWorkspa
 			return fmt.Errorf(strings.TrimSpace(
 				errMigrateMulti), name, opts.SourceType, opts.DestinationType, err)
 		}
+	}
+
+	// After migrating multiple workspaces, we want to ensure that a workspace is
+	// set or we prompt the user to set a workspace.
+	err = m.selectWorkspace(opts.Destination)
+	if err != nil {
+		return err
 	}
 
 	return nil
