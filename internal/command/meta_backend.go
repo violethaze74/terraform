0c082f84b75940c2ff81b9a46f385b98e111986f
Barrett Clark <barrett.clark@hashicorp.com>
2021-10-18 16:02:45 -0500

    More Terraform Cloud UX changes
    * Update e2e tests to specify Terraform Cloud
    * Update more of the terraform command flow to specify Terraform Cloud

--- internal/command/meta_backend.go
+++ internal/command/meta_backend.go
@@ -942,7 +942,11 @@ func (m *Meta) backend_C_r_S_changed(c *configs.Backend, cHash int, sMgr *clista
 
 	// no need to confuse the user if the backend types are the same
 	if s.Backend.Type != c.Type {
-		m.Ui.Output(strings.TrimSpace(fmt.Sprintf(outputBackendMigrateChange, s.Backend.Type, c.Type)))
+		output := fmt.Sprintf(outputBackendMigrateChange, s.Backend.Type, c.Type)
+		if c.Type == "cloud" {
+			output = fmt.Sprintf(outputBackendMigrateChangeCloud, s.Backend.Type)
+		}
+		m.Ui.Output(strings.TrimSpace(output))
 	}
 
 	// Grab the existing backend
@@ -1001,8 +1005,12 @@ func (m *Meta) backend_C_r_S_changed(c *configs.Backend, cHash int, sMgr *clista
 	}
 
 	if output {
-		m.Ui.Output(m.Colorize().Color(fmt.Sprintf(
-			"[reset][green]\n"+strings.TrimSpace(successBackendSet), s.Backend.Type)))
+		// By now the backend is successfully configured.  If using Terraform Cloud, the success
+		// message is handled as part of the final init message
+		if _, ok := b.(*cloud.Cloud); !ok {
+			m.Ui.Output(m.Colorize().Color(fmt.Sprintf(
+				"[reset][green]\n"+strings.TrimSpace(successBackendSet), s.Backend.Type)))
+		}
 	}
 
 	return b, diags
@@ -1294,6 +1302,10 @@ above, resolve it, and try again.
 
 const outputBackendMigrateChange = `
 Terraform detected that the backend type changed from %q to %q.
+`
+
+const outputBackendMigrateChangeCloud = `
+Terraform detected that the backend type changed from %q to Terraform Cloud.
 `
 
 const outputBackendMigrateLocal = `
